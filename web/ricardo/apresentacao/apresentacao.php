04de0137481693a6319af5c39f71b16193487d8a
Misterduplo <113913840+Misterduplo@users.noreply.github.com>
2022-09-26 12:44:28 +0100

    Add files via upload

---
+++ web/ricardo/apresentacao/apresentacao.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+<meta charset="UTF-8" >
+  <head>
+       text_alne= <title><h1>Apresentação / Ponto de situação da Pape</h1></title>
+       <style>
+        div { 
+            text-align: center;
+        }
+    </style>
+  </head>
+<body>
+    <div>
+       <img src="didi2.jpg" alt=""> 
+    </div>
+     <h2>Ricardo David</h2> 
+
+     <br><h2> Eu Sou o Ricardo Tenho 19 anos estou a estudar e em tenpos vagos a trabalahar ou a treinar <br>
+     Jogo no club Unão Idanhençe a possiçao esta  a ser difenida mas ao que pareçe vou voltar a ser medio centro <br>
+     Estou Ancioso por esta Epoca ,sou tambem integrante do curso Tecnico de informatica e gestão <br>
+     Minha turma e a melhor cada um com a sua pancada mas nao trucaria nenho nenos o lourenço esse posso vendelo <br>eeeeee
+</body>
